8581bbd05a3959259541d4c4530acf932576ee4d
Tyler Newman <tylerjnewman18@gmail.com>
2024-11-18 13:16:16 -0800

    Add more types for rate limiter

--- lib/rate-limiter.ts
+++ lib/rate-limiter.ts
@@ -1,10 +1,15 @@
 import { Ratelimit } from '@upstash/ratelimit'
 import { Redis } from '@upstash/redis'
 
+type Duration = `${number} ms` | `${number} s` | `${number} m` | `${number} h` | `${number} d` | `${number}ms` | `${number}s` | `${number}m` | `${number}h` | `${number}d`
+
+const RATE_LIMIT_WINDOW = (process.env.RATE_LIMIT_WINDOW || '1 h') as Duration
+const RATE_LIMIT_MAX = parseInt(process.env.RATE_LIMIT_MAX || '50');
+
 // Create a new ratelimiter that allows 50 requests per hour
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(50, '1 h'),
+  limiter: Ratelimit.slidingWindow(RATE_LIMIT_MAX, RATE_LIMIT_WINDOW),
   analytics: true, // Optional: Enable analytics
   prefix: '@upstash/ratelimit',
 })
